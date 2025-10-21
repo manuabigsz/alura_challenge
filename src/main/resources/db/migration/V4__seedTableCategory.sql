5591a8a37488c48e20d7c31c06f40c7dcb22ad58
manuabigsz <manuelaossanes@hotmail.com>
2025-10-21 19:55:05 -0300

    feat(db): adds initial seeder for categories

---
+++ src/main/resources/db/migration/V4__seedTableCategory.sql
@@ -0,0 +1,10 @@
+INSERT INTO Category (name, code, color, `order`)
+VALUES 
+('PROGRAMAÇÃO', 'programacao', '#00C86F', 1),
+('FRONT-END', 'front-end', '#6BD1FF', 2),
+('DATA SCIENCE', 'data-science', '#9CD33B', 3),
+('INTELIGÊNCIA ARTIFICIAL', 'inteligencia-artificial', '#7B71FF', 4),
+('DEVOPS', 'devops', '#F16165', 5),
+('UX & DESIGN', 'ux-design', '#DC6EBE', 6),
+('MOBILE', 'mobile', '#FFBA05', 7),
+('INOVAÇÃO E GESTÃO', 'inovacao-gestao', '#FF8C2A', 8);
